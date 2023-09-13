5372466e71ae110a529f1227ae7b9b1eec96d918
parthesh8 <parthesh.gupta@delhivery.com>
2023-09-13 13:42:05 +0530

    dashboard and home skeleton

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Set the default route to Home
+  { path: 'projects', component: ProjectsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

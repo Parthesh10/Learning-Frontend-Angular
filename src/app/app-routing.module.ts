bee2bba60ed07924e46c1f09a67c4ef2a245778b
parthesh8 <parthesh.gupta@delhivery.com>
2023-09-28 10:33:31 +0530

    changed folder structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Set the default route to Home
+  { path: 'projects', component: ProjectsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
